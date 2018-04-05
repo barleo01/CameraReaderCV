7a4af9af669a5bb38dd95f4d26943248501809d2
barleo01 <barleo01@hotmail.com>
2018-04-05 19:36:58 +0200

    Pump control implemented

---
+++ controlHW.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""controlHW.py: Control Hardware."""
+
+__author__      = "Léonard Barras"
+__copyright__   = "Copyright 2018"
+
+
+
+def StartPump():
+    print("start pump")
+    
+def StopPump():
+    print("stop pump")
